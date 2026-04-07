9b178fd230c89b343e03bf0a1a8c4a439f7ec442
Noora Liv <j.czinzoll@ingressy.de>
2026-04-07 17:19:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import time
+
 from sensors import battery_ampere
+from sensors import battery_voltage
 
 def main():
     print("===Testmode ===")
     chan0 = battery_ampere.initmcp()
-    battery_ampere.readampere(chan0)
+    chan1 = battery_voltage.initmcp()
+    while True:
+        battery_ampere.readampere(chan0)
+        battery_voltage.readvoltage(chan1)
+        time.sleep(0.5)
 
 
 
