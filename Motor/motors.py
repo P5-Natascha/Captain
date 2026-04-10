e1a20228d793a9b46f0ef395243363f3dbafe7f2
ingressy <j.czinzoll@ingressy.de>
2026-04-10 10:12:41 +0200

    Käfer beheben

---
+++ Motor/motors.py
@@ -0,0 +1,76 @@
+"""
+written by Jannik Czinzoll
+"""
+import digitalio
+import pwmio
+import time
+
+import pins
+
+
+def vorwaerts(t: int, speed: int) -> None:
+    dir2 = digitalio.DigitalInOut(pins.DIR2)
+    dir2.direction = digitalio.Direction.OUTPUT
+
+    pwm2 = pwmio.PVMOut(pins.PWM2, frequency=1000, duty_cycle=0)
+
+    # true = vorwaerts
+    dir2.value = True
+    pwm2.duty_cycle = int(speed/100*65535) # 16-bit: 0–65535
+
+    time.sleep(t)
+
+    #stops and deinit things
+    pwm2.duty_cycle = 0
+    pwm2.deinit()
+    dir2.deinit()
+
+def rueckwaerts(t: int, speed: int) -> None:
+    dir2 = digitalio.DigitalInOut(pins.DIR2)
+    dir2.direction = digitalio.Direction.OUTPUT
+
+    pwm2 = pwmio.PVMOut(pins.PWM2, frequency=1000, duty_cycle=0)
+
+    # true = vorwaerts
+    dir2.value = False
+    pwm2.duty_cycle = int(speed/100*65535) # 16-bit: 0–65535
+
+    time.sleep(t)
+
+    #stops and deinit things
+    pwm2.duty_cycle = 0
+    pwm2.deinit()
+    dir2.deinit()
+
+def links(t: int, speed: int) -> None:
+    dir1 = digitalio.DigitalInOut(pins.DIR2)
+    dir1.direction = digitalio.Direction.OUTPUT
+
+    pwm1 = pwmio.PVMOut(pins.PWM1, frequency=1000, duty_cycle=0)
+
+    # true = vorwaerts
+    dir1.value = False
+    pwm1.duty_cycle = int(speed/100*65535) # 16-bit: 0–65535
+
+    time.sleep(t)
+
+    #stops and deinit things
+    pwm1.duty_cycle = 0
+    pwm1.deinit()
+    dir1.deinit()
+def rechts(t: int, speed: int):
+    dir1 = digitalio.DigitalInOut(pins.DIR2)
+    dir1.direction = digitalio.Direction.OUTPUT
+
+    pwm1 = pwmio.PVMOut(pins.PWM1, frequency=1000, duty_cycle=0)
+
+    # true = vorwaerts
+    dir1.value = True
+    pwm1.duty_cycle = int(speed/100*65535) # 16-bit: 0–65535
+
+    time.sleep(t)
+
+    #stops and deinit things
+    pwm1.duty_cycle = 0
+    pwm1.deinit()
+    dir1.deinit()
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import time
 
 from Sensoren import ADC
-from motors import deathrun
+from Motor import motors
 
 def main():
     print("===Testmode ===")
     adc = ADC.ADC()
 
-    adc.get_ampere(0)
-    adc.get_12voltage(1)
+    print(adc.get_ampere(0) + "A")
+    print(adc.get_12voltage(1) + "V")
 
 
-    print("===Dreht Motoren in 30Sek!===")
-    time.sleep(30)
-    deathrun.deathrun(5,True,25)
+    print("===Dreht Motoren in 5Sek!===")
+    time.sleep(5)
+
+    motors.vorwaerts(30, 100)
+
 
     adc.de_ADC()
     print("===Testmode fertig!===")
