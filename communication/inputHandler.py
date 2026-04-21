c8667b9807ce3e04d645bd5e65d0cd498bb210c4
ingressy <j.czinzoll@ingressy.de>
2026-04-21 09:02:55 +0200

    Motoren Heile gemacht und ADC Faktor für Volt angepasst.

--- communication/inputHandler.py
+++ communication/inputHandler.py
@@ -1,4 +1,4 @@
-from comps.motors.motors import vorwaerts, rueckwaerts, stop, rechts, links, keineAhnungDigga
+from comps.motors.motors import Motors
 import globals
 import logging
 
@@ -7,31 +7,32 @@ LENKUNG = 75
 DEADZONE_POS = 1950
 DEADZONE_NEG = 1750
 def inputHandler(x,y):
+        motors = Motors()
         if globals.current_mode == "1" or globals.current_mode == "2":
             if y > DEADZONE_POS:
                 speed = ((y - DEADZONE_POS) / (4095 - DEADZONE_POS)) * VELOCITY
                 speed = max(0.0, min(100.0, speed))
-                vorwaerts(speed)
+                motors.vorwaerts(speed)
             elif y < DEADZONE_NEG:
                 speed = ((DEADZONE_NEG - y) / DEADZONE_NEG) * VELOCITY
                 speed = max(0.0, min(100.0, speed))
-                rueckwaerts(speed)
+                motors.rueckwaerts(speed)
             else:
-                stop()
+                motors.stop()
 
             if x > DEADZONE_POS:
                 speed = ((x - DEADZONE_POS) / (4095 - DEADZONE_POS)) * LENKUNG
                 speed = max(0.0, min(100.0, speed))
-                rechts(speed)
+                motors.rechts(speed)
             elif x < DEADZONE_NEG:
                 speed = ((DEADZONE_NEG - x) / DEADZONE_NEG) * LENKUNG
                 speed = max(0.0, min(100.0, speed))
-                links(speed)
+                motors.links(speed)
             else:
-                keineAhnungDigga()
+                motors.stoplenkung()
         else:
-            stop()
-            keineAhnungDigga()
+            motors.stop()
+            motors.stoplenkung()
             logging.debug(f"Mache nix weil current_mode: {globals.current_mode}")
 def msgHanlder(msg):
     if ":" in msg:
